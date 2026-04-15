22d55bcbbc319f2c4740ab6b6b964cd59da210c1
Sanjay Nagaraj <sanjaynagraj@gmail.com>
2026-04-14 19:03:09 -0700

    Add Google sign in for iOS devices

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,17 @@
 import Flutter
+import GoogleSignIn
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
 
+  override func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    guard let url = URLContexts.first?.url else {
+      super.scene(scene, openURLContexts: URLContexts)
+      return
+    }
+    if GIDSignIn.sharedInstance.handle(url) {
+      return
+    }
+    super.scene(scene, openURLContexts: URLContexts)
+  }
 }
